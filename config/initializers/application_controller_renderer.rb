56bc836497e046da22dbe1247b521aa4fd9529e9
jun56br <junwithpma@hotmail.com>
2021-01-26 20:15:18 +0900

    フォーム等調整

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,16 @@
 #     https: false
 #   )
 # end
+WillPaginate::ViewHelpers.pagination_options = {
+  :class          => 'pagination',
+  :previous_label => '&laquo;前へ',
+  :next_label     => '次へ&raquo;',
+  :inner_window   => 4, # links around the current page
+  :outer_window   => 1, # links around beginning and end
+  :separator      => ' ', # single space is friendly to spiders and non-graphic browsers
+  :param_name     => :page,
+  :params         => nil,
+  :renderer       => 'WillPaginate::ViewHelpers::LinkRenderer',
+  :page_links     => true,
+  :container      => true
+}
